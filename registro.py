2ad856ae3983f015a3bd50f6616cfc1b9ffd6f3b
Julia Fonseca <42375312+jufs128@users.noreply.github.com>
2021-11-25 23:51:38 -0300

    Update registro.py

--- registro.py
+++ registro.py
@@ -1,20 +1,10 @@
 from dataclasses import dataclass
 
-from nota import Nota
 from midia import *
-from comentario import Comentario
 
 @dataclass
 class Registro():
-    nota: Nota
+    nota: float
     midia: Midia
-    comentario: Comentario
-    
-def main():
-    nota = Nota(10)
-    midia = Livro("Teste", "TesteGenero", "TesteAutor", 200)
-    comentario = Comentario("Um comentário teste")
-
-    registro = Registro(nota, midia, comentario)
-    
-    
+    comentario: str
+    ja_consumiu: bool
