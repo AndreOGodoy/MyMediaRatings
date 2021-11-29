2efedddf2d1efc0025954ecfbb1f43ed6431614c
Andre Godoy <godoy.andre@hotmail.com>
2021-11-29 12:27:42 -0300

    View: Implementa cache para evitar uso excessivo de memória

--- views.py
+++ views.py
@@ -4,23 +4,26 @@ from typing import List
 
 from base_dados import Base_Midias
 
-class View():
-    _data: List[pd.DataFrame]
-    _filtros: [str]
-
-    _instancia: Base_Midias
+# Esta classe é utilizada para organização interna
+#
+# Ela é instanciada no momento que o módulo for importado pelo cliente,
+# fazendo com que carregue os dados do banco de dados
+#
+# Seu objetivo é impedir que haja um acesso ao DB, e o carregamento de seu
+# conteúdo à memória, à cada nova construção de View()
+class _ViewCache():
+    _instancia = pd.DataFrame
 
     def __init__(self):
-        self._filtros = []
         self._instancia = Base_Midias()
-        self._sincroniza_db()
 
-    def _sincroniza_db(self):
+    def obtem_dbs(self) -> List[pd.DataFrame]:
         # O código abaixo tem como objetivo obter os dataframes, atributos de Base_Midias, sem ter
         # que os referenciá-los diretamente. Assim, caso haja mudança no nome e/ou quantidade dos mesmos,
         # este método não será afetado
 
         self._instancia.atualiza_arquivos()
+
         # Obtem todos os atributos
         db_attr = dir(self._instancia)
 
@@ -30,7 +33,24 @@ class View():
         # Remove os métodos, deixando apenas os 'data attributes'
         db_data_attr = filter(lambda prop: not callable(getattr(self._instancia, prop)), db_attr_sem_especiais)
 
-        self._data =  [getattr(self._instancia, df_name) for df_name in db_data_attr]
+        # Retorna os 'data_attributes', que são os DataFrames
+        return [getattr(self._instancia, df_name) for df_name in db_data_attr]
+
+CACHE = _ViewCache()
+
+class View():
+    _data: List[pd.DataFrame]
+    _filtros: [str]
+
+    _instancia: _ViewCache
+
+    def __init__(self):
+        self._filtros = []
+        self._instancia = CACHE
+        self._sincroniza_db()
+
+    def _sincroniza_db(self):
+        self._data = self._instancia.obtem_dbs()
 
     @property
     def filtros(self):
