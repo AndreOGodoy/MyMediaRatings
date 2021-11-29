05779c0351a679a36e2654832d3e7112ea36cb20
Andre Godoy <godoy.andre@hotmail.com>
2021-11-29 12:05:18 -0300

    View: Realiza integração com banco de dados

--- views.py
+++ views.py
@@ -7,14 +7,30 @@ from base_dados import Base_Midias
 class View():
     _data: List[pd.DataFrame]
     _filtros: [str]
-    
+
+    _instancia: Base_Midias
+
     def __init__(self):
-        self._sincroniza_db()
         self._filtros = []
+        self._instancia = Base_Midias()
+        self._sincroniza_db()
 
     def _sincroniza_db(self):
-        # Conexão com DB ainda não integrada
-        self._data = [pd.DataFrame()]
+        # O código abaixo tem como objetivo obter os dataframes, atributos de Base_Midias, sem ter
+        # que os referenciá-los diretamente. Assim, caso haja mudança no nome e/ou quantidade dos mesmos,
+        # este método não será afetado
+
+        self._instancia.atualiza_arquivos()
+        # Obtem todos os atributos
+        db_attr = dir(self._instancia)
+
+        # Remove os métodos especiais como __init__ e __repr__
+        db_attr_sem_especiais = filter(lambda attr: not attr.startswith('__') , db_attr)
+
+        # Remove os métodos, deixando apenas os 'data attributes'
+        db_data_attr = filter(lambda prop: not callable(getattr(self._instancia, prop)), db_attr_sem_especiais)
+
+        self._data =  [getattr(self._instancia, df_name) for df_name in db_data_attr]
 
     @property
     def filtros(self):
