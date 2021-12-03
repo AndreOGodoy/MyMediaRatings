64165c1913bae60185106b20ff13b39782c8501c
Andre Godoy <godoy.andre@hotmail.com>
2021-12-03 19:51:38 -0300

    View: Melhoria nos testes

--- testes_views.py
+++ testes_views.py
@@ -42,15 +42,53 @@ class TestViews(TestCase):
     def setUp(self):
         self.view = View()
 
-    def test_numero_filtros_possiveis_nova_view(self):
-        filtros_possiveis = self.view.obtem_filtros_possiveis()
-        self.assertEqual(len(filtros_possiveis), 17)
+    def test_nova_view_composicao_vazia(self):
+        self.assertTrue(self.view._composicao.empty)
 
-    def test_filtra_por_aplica_filtro(self):
-        self.view.filtra_por('duracao')
+    def test_nova_view_sem_filtros(self):
+        numero_filtros = len(self.view.filtros)
+        self.assertTrue(numero_filtros == 0)
+
+    # Método utilitário
+    def arrays_sao_iguais(self, arr1: np.ndarray, arr2: np.ndarray):
+        return (arr1 == arr2).all()
+
+    # Obtem as colunas do atributo _data da view. Evita acesso a elementos
+    # não-interface de view
+    def colunas_view(self, view: View):
+        como_string = view.__repr__()
+        primeira_linha = como_string.split('\n')[0]
+
+        como_lista = primeira_linha.split(' ')
+
+        # Remove detalhe de impressão do Pandas
+        nomes = filter(lambda x: x.replace('\\', '') != '', como_lista)
+
+        return list(nomes)
+
+    def test_conteudo_reflete_filtros(self):
+        filtros_aplicados = ['tipo_midia', 'nome', 'autor', 'nota']
+        self.view.filtros = filtros_aplicados[:2]
+        for filtro in filtros_aplicados[2:]:
+            self.view.filtra_por(filtro)
+
+        colunas_obtidas = self.view._composicao.columns
+        colunas_esperadas = filtros_aplicados
+        self.assertTrue(self.arrays_sao_iguais(colunas_obtidas, filtros_aplicados))
+
+    def test_filtros_possiveis_igual_total_colunas_db(self):
+        filtros_possiveis = self.view.obtem_filtros_possiveis()
+        colunas_db = cria_db_teste().columns
+
+        self.assertTrue(self.arrays_sao_iguais(colunas_db, filtros_possiveis))
+
+    def test_filtra_por_aplica_devido_filtro(self):
+        filtro = 'duracao'
+
+        self.view.filtra_por(filtro)
         filtros_aplicados = self.view.filtros
 
-        self.assertEqual(filtros_aplicados, ['duracao'])
+        self.assertEqual(filtros_aplicados, [filtro])
 
     def test_define_filtro_inexistente(self):
         with self.assertRaises(FiltroInexistenteException):
@@ -81,10 +119,4 @@ class TestViews(TestCase):
 
         self.assertEqual(len(filtros_possiveis_pre_redef), len(filtros_possiveis_pos_def) + 1)
 
-    def test_numero_filtros_possiveis_apos_nova_aplicacao(self):
-        filtros_possiveis_inicial = self.view.obtem_filtros_possiveis()
-        self.view.filtros = ['genero']
 
-    def test_numero_filtros_aplicados_nova_view(self):
-        filtros = self.view.filtros
-        self.assertEqual(len(filtros), 0)
